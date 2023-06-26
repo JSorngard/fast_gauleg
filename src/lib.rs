fbd4b3c9d48cacce10717cd850b32ddf0838715e
Johanna Sörngård <jsorngard@gmail.com>
2023-06-26 19:42:35 +0200

    Fun simplification of a test

--- src/lib.rs
+++ src/lib.rs
@@ -52,7 +52,7 @@
 //! for n in 0..=19 {
 //!     assert_relative_eq!(
 //!         integrator.integrate(-1.0, 1.0, |x| x.powf(n.into())),
-//!         if n % 2 == 0 {2.0} else {0.0} / f64::from(n + 1),
+//!         2.0 * f64::from(n % 2 == 0) / f64::from(n + 1),
 //!         epsilon = 1e-13,
 //!     );
 //! }
